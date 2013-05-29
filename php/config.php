7016400532ffa0a12b32f752179208175d9c80aa
moizest89 <moises.gilberto@gmail.com>
2013-05-29 00:15:36 -0600

    update

--- php/config.php
+++ php/config.php
@@ -1,3 +1,3 @@
 <?php
-    $connection = mysql_connect('localhost', 'root', '');
+    $connection = mysql_connect('198.199.75.241', 'root', '');
     mysql_select_db('new_contact_form_ejje', $connection);
